3905c532e8b024741059559b3460f10a19635868
enithHassa <enithhaasa18@gmail.com>
2025-04-24 13:16:35 +0530

    importing the CourseManager file path and  delete other application file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Logout from './components/users/Logout';
 import EditProfile from './components/users/EditProfile';
 import Signup from './components/users/Signup';
 import SkillsharePost from './components/skillpost/SkillsharePost';
+import CourseManager from './components/courses/CourseList';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/update" element={<EditProfile />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/courses" element={<CourseManager />} />
         
       </Routes>
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} />
