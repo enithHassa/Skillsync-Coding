4cd2f2be299e0fb74540cda32dba4f6db4c9d05d
enithHassa <enithhaasa18@gmail.com>
2025-04-25 13:13:57 +0530

    Resolved conflicts changes
    committing after resolving the conflicts changes with comment merging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,26 @@ import Profile from './components/users/Profile';
 import Logout from './components/users/Logout';
 import EditProfile from './components/users/EditProfile';
 import Signup from './components/users/Signup';
+import SkillsharePost from './components/skill-posts/SkillsharePost';
+import CourseManager from './components/courses/CourseList';
+import ProgressPage from './components/learning-progress/pages/ProgressPage';
+import Comment from './components/interactivity/Comment';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/s" element={<SkillsharePost />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/update" element={<EditProfile />} />
         <Route path="/logout" element={<Logout />} />
+
+        <Route path="/s" element={<SkillsharePost />} />
+        <Route path="/courses" element={<CourseManager />} />
+        <Route path="/progress" element={<ProgressPage />} />
+        <Route path="/comment" element={<Comment />} />
 
       </Routes>
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} />
